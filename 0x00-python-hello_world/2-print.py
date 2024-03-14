a613a7b86daacde9ed9446e18b9d7f2b033d5025
sean15535 <sean15535@gmail.com>
2024-03-13 17:30:07 -0700

    completed task 2 and 3

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-print("\"Programming is like building a multilingual puzzle")
+print("\"Programming is like building a multilingual puzzle\n")
+
 
 
 
--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 number = 98
-print(f"{number:d} Battery street")
+print(f"{number} Battery street\n")
+
+
+
 
 
 
